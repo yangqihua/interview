2b733737a9222306b47657a3df89e0dca91e906f
yangqihua <904693433@qq.com>
2018-05-02 22:21:42 +0800

    ...

---
+++ src/main/java/cn/yang/interview/async/Thead.java
@@ -0,0 +1,13 @@
+package cn.yang.interview.async;
+
+/**
+ * User: yangqihua
+ * Date: 2018-04-26
+ * Time: 下午5:54
+ */
+public class Thead {
+
+	public void base(){
+	}
+
+}
---
+++ src/main/java/cn/yang/interview/java_base/Map.java
@@ -0,0 +1,14 @@
+package cn.yang.interview.java_base;
+
+import java.util.HashMap;
+
+/**
+ * User: yangqihua
+ * Date: 2018-05-02
+ * Time: 下午5:08
+ */
+public class Map {
+	public static void main(String[] args){
+		HashMap<String, String> map = new HashMap<String,String>();
+	}
+}
